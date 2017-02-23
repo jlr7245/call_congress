3f770ceeddcaa6ec67a1673a5a58f07b034cc547
J Silverstein <jess.pebble@gmail.com>
2017-02-23 14:43:21 -0500

    working on districts

--- routes/api/ext.js
+++ routes/api/ext.js
@@ -5,7 +5,7 @@ const districtHelpers = require('./geo/districtHelpers');
 
 extRouter.post('/geo', districtHelpers.coord2pol, (req, res, next) => {
   // res.send({st: res.locals.stateAbb, dst: res.locals.distNum})
-  res.send({message: 'in progress'});
+  res.send({resultArray: res.locals.districtArr});
 })
 
 module.exports = extRouter;
--- routes/api/geo/districtHelpers.js
+++ routes/api/geo/districtHelpers.js
@@ -4,10 +4,25 @@ const dstkAXIOS = axios.create({
   baseURL: 'http://www.datasciencetoolkit.org/coordinates2politics',
 })
 
+
+function extractDistrict(arr) {
+  let holder = [];
+  let theResult = [];
+  for (let obj of arr) {
+    if (obj.type === 'constituency' && holder.indexOf(obj.code) == -1) {
+      holder.push(obj.code);
+      theResult.push([parseInt(obj.code.split('_')[1]), obj.name.split(', ')[1]] );
+    }
+  }
+  return theResult;
+}
+
 function coord2pol(req, res, next) {
   console.log(req.body.latlng);
   dstkAXIOS.get(`/${req.body.latlng}`)
-    .then((result) => console.log(result.data[0].politics))
+    .then((result) => {
+      res.locals.districtArr = extractDistrict(result.data[0].politics);
+    })
     .catch((err) => console.log(err));
   next();
 }
