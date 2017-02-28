cee2475e809f4949ae53a00a12e0e70464c340e1
J Silverstein <jess.pebble@gmail.com>
2017-02-27 23:04:36 -0500

    🤔

--- routes/api/pol/dashHelper.js
+++ routes/api/pol/dashHelper.js
@@ -3,7 +3,7 @@ const models = require('../../../db/models/index.js');
 const allLegs = require('./sunlight-array');
 
 function getLegsFromDb(req,res,next) {
-  models.sequelize.query('SELECT "LegsWatched"."bioguide_id" FROM "LegsWatcheds" JOIN "Users" ON "User"."id" = "LegsWatched"."belongs_to" WHERE "User"."id" = :id', {
+  models.sequelize.query('SELECT "LegsWatcheds"."bioguide_id" FROM "LegsWatcheds" JOIN "Users" ON "Users"."id" = "LegsWatcheds"."belongs_to" WHERE "Users"."id" = :id', {
     replacements: { id: parseInt(req.params.id) },
     type: models.sequelize.QueryTypes.SELECT
   }).then((legs) => {
