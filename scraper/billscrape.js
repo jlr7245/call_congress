19c40611e40c79519cc6f517e9f8742cab7fb2d8
J Silverstein <jess.pebble@gmail.com>
2017-03-13 00:07:05 -0400

    setting up scraper on a timer

--- scraper/billscrape.js
+++ scraper/billscrape.js
@@ -18,10 +18,5 @@ this file will run every couple of hours and perform a number of tasks
 // testing out the basic concept
 function tester() {
   console.log('hello world');
-  axios.get('http://localhost:3000/test')
-    .then((result) => {
-      console.log(result.data);
-    })
 }
 tester();
-// `npm run scrape` outputs hello world and the result from that API call 💪
