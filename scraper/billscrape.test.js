2c703133d79e03a1546611f9438d10ca23520022
J Silverstein <jess.pebble@gmail.com>
2017-03-13 09:03:22 -0400

    playing with tests

--- scraper/billscrape.test.js
+++ scraper/billscrape.test.js
@@ -1,5 +1,16 @@
 const assert = require('assert');
 
-describe('billscrape', () => {
-  it('passes', () => {});
+describe('level -2', () => {
+  describe('setup test', () => {
+    it('#passes', () => {
+      assert.equal(1, 1);
+    });
+    it('#has a second test');
+  });
+});
+
+describe('level -1', () => {
+  describe('subtest 1', () => {
+    it('#passes', () => {});
+  });
 });
